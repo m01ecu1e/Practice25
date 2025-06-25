9ec7a61fb3060a749b6d2d3ec10937ad00aad1e1
Daniil <Danya@gmail.com>
2025-06-25 19:17:07 +0300

    switch-case

--- Practice25/Sorter.cpp
+++ Practice25/Sorter.cpp
@@ -6,18 +6,31 @@ void heapify(std::vector<int>& arr, int n, int i, long& swapCount) {
     int left = 2 * i + 1;
     int right = 2 * i + 2;
 
-    if (left < n && arr[left] > arr[largest]) {
+    // Заменяем if на switch-case
+    switch (left < n && arr[left] > arr[largest]) {
+    case true:
         largest = left;
+        break;
+    default:
+        break;
     }
 
-    if (right < n && arr[right] > arr[largest]) {
+    switch (right < n && arr[right] > arr[largest]) {
+    case true:
         largest = right;
+        break;
+    default:
+        break;
     }
 
-    if (largest != i) {
+    switch (largest != i) {
+    case true:
         std::swap(arr[i], arr[largest]);
         swapCount++;
         heapify(arr, n, largest, swapCount);
+        break;
+    default:
+        break;
     }
 }
 
@@ -25,12 +38,12 @@ long heapsort(std::vector<int>& arr) {
     long swapCount = 0;
     int n = arr.size();
 
-    
+    // Build max heap
     for (int i = n / 2 - 1; i >= 0; i--) {
         heapify(arr, n, i, swapCount);
     }
 
-    
+    // Extract elements from heap
     for (int i = n - 1; i > 0; i--) {
         std::swap(arr[0], arr[i]);
         swapCount++;
