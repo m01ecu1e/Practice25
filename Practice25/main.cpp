38de50078ae397fcc4534c2a841c1052a067c9ca
Brenix22 <mrminecraft2845>
2025-06-27 19:01:14 +0300

    Убрал лишние функции

--- Practice25/main.cpp
+++ Practice25/main.cpp
@@ -9,78 +9,88 @@
 
 using namespace std;
 
-void clearInputBuffer() {
-    cin.clear();
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-}
-
-int getIntInput(const string& prompt, int min = INT_MIN, int max = INT_MAX) {
-    int value;
-    while (cout << prompt && (!(cin >> value) || value < min || value > max)) {
-        cout << "Ошибка, введите целое число";
-        if (min != INT_MIN || max != INT_MAX) cout << " [" << min << ".." << max << "]";
-        cout << ".\n";
-        clearInputBuffer();
-    }
-    clearInputBuffer();
-    return value;
-}
-
 void runConsoleUI() {
     vector<int> arr;
     string filename;
 
     while (true) {
-        cout << "\n==== ГЛАВНОЕ МЕНЮ ====\n"
-            << "1. Сгенерировать массив\n"
-            << "2. Отсортировать существующий массив\n";
+        cout << "\n==== ГЛАВНОЕ МЕНЮ ====\n";
+        cout << "1. Сгенерировать массив\n";
+        cout << "2. Отсортировать существующий массив\n";
+        cout << "0. Выход\n";
 
-        int choice = getIntInput("Выберите вариант (0-2): ", 0, 2);
+        int choice;
+        cout << "Выберите вариант (0-2): ";
+        cin >> choice;
 
-        switch (choice) {
-        case 1: {
-            int size = getIntInput("Введите размер массива: ", 1);
+        if (cin.fail()) {
+            cin.clear();
+            cin.ignore(1000, '\n');
+            cout << "Ошибка ввода. Введите число от 0 до 2.\n";
+            continue;
+        }
+
+        cin.ignore();
+
+        if (choice == 0) break;
+
+        if (choice == 1) {
+            int size;
+            cout << "Введите размер массива: ";
+            cin >> size;
+
+            if (cin.fail() || size <= 0) {
+                cin.clear();
+                cin.ignore(1000, '\n');
+                cout << "Ошибка: размер должен быть положительным числом.\n";
+                continue;
+            }
+
+            cin.ignore();
+
             arr = generateRandomArray(size);
 
             cout << "Введите название файла для сохранения: ";
             getline(cin, filename);
-
             if (writeArrayToFile(filename, arr)) {
-                cout << "Массив сохранен в файл " << filename << "\n";
+                cout << "Массив сохранён в файл " << filename << "\n";
             }
             else {
                 cout << "Ошибка при сохранении файла!\n";
             }
-            break;
-        }
 
-        case 2: {
+        }
+        else if (choice == 2) {
             cout << "Введите название файла для чтения: ";
             getline(cin, filename);
-
             if (readArrayFromFile(filename, arr)) {
-                cout << "Файл успешно прочитан\n";
+                cout << "Файл успешно прочитан.\n";
 
                 SortResult result = sortAndMeasureHeapsort(arr);
                 string sortedFilename = filename + "_sorted";
 
                 if (writeArrayToFile(sortedFilename, arr)) {
-                    cout << "Отсортированный массив сохранен в файл " << sortedFilename << "\n";
+                    cout << "Отсортированный массив сохранён в файл " << sortedFilename << "\n";
                 }
                 else {
                     cout << "Ошибка при сохранении отсортированного файла!\n";
                 }
 
                 cout << "Массив отсортировался за " << result.durationSec
-                    << " секунд, за " << result.swapCount << " операций\n";
+                    << " секунд, за " << result.swapCount << " операций.\n";
+
             }
             else {
                 cout << "Ошибка при чтении файла!\n";
             }
-            break;
+
         }
+        else {
+            cout << "Неверный пункт меню.\n";
         }
     }
+
+    cout << "Выход из программы.\n";
 }
 
 int main() {
